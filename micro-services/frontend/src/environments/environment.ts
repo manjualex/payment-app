adaf4cd20ed66b2575d978813aaeeb84c2fea739
Ubuntu <ubuntu@ip-172-31-47-73.ec2.internal>
2025-01-17 17:00:20 +0000

    Update environment.ts, logging_config, and nginx configuration

--- micro-services/frontend/src/environments/environment.ts
+++ micro-services/frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://54.81.153.151:80/api',
-    authServiceUrl: 'http://54.81.153.151:80/api/auth',
-    paymentServiceUrl: 'http://54.81.153.151:80/api/payments'
+    apiUrl: 'http://54.146.39.93:80/api',
+    authServiceUrl: 'http://54.146.39.93:80/api/auth',
+    paymentServiceUrl: 'http://54.146.39.93:80/api/payments'
   };
